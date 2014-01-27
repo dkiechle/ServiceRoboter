4fdd2b21dce4d2590e04864b4aa486fa24e5d315
NFreund <nicolefreund@gmx.net>
2014-01-27 16:55:19 +0100

    Fehler bei Feuervermeidung gebessert

--- src/horst/Lokalisierung.java
+++ src/horst/Lokalisierung.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import lejos.nxt.Button;
 
 class Lokalisierung{
+	private ArrayList<Koordinaten> coords;
 	private byte mX=-1;
 	private byte mY=-1;
 	private Map map;
@@ -18,12 +19,14 @@ class Lokalisierung{
 	byte getDistance(){	// dist to next Wall in current Dir
 		if (map.getPosRi()==Richtung.EAST){
 			for (byte i=1; i < (map.getlength()-map.getPosX()); i++){
-				if (map.isFree(map.getPosX()+i,map.getPosY())){	return (byte)(i-1);}
+				Koordinaten coord=new Koordinaten(i, (byte)map.getPosY());
+				if (map.isFree(map.getPosX()+i,map.getPosY())||coords.contains(coord)){	return (byte)(i-1);}
 			}
 		}
 		if (map.getPosRi()==Richtung.SOUTH){
 			for (byte i=1; i < (map.getlength()-map.getPosY()); i++){
-				if (map.isFree(map.getPosX(),map.getPosY()+i)){	return (byte)(i-1);}
+				Koordinaten coord=new Koordinaten((byte)map.getPosY(),i);
+				if (map.isFree(map.getPosX(),map.getPosY()+i)||coords.contains(coord)){	return (byte)(i-1);}
 			}
 		}
 		return -1;
@@ -33,7 +36,7 @@ class Lokalisierung{
 	 */
 	boolean isFire(){
 		byte mF=1;
-		ArrayList <Koordinaten> coords=new ArrayList<Koordinaten>();
+		coords=new ArrayList<Koordinaten>();
 		for (byte x=0; x<map.getlength();x++){
 			for(byte y=0; y<map.getlength();y++){
 				if (mF<map.getFeuer(x,y) ){
@@ -78,42 +81,57 @@ class Lokalisierung{
 		switch(dir){
 					//@toDo: maximum==0 testen, mit go Way
 			case EAST:
-			x=Math.min(((map.getPosX()+map.getlength())/2),getDistance());
-			for (byte i=0; i<x;i++){
-				w.add(Richtung.EAST);
-			}
-			goWay(w);
-			return;
+				x=Math.min(((map.getPosX()+map.getlength())/2),getDistance());
+				for (byte i=0; i<x;i++){
+					w.add(Richtung.EAST);
+				}
+				goWay(w);
+				return;
 			
 			case SOUTH:
-			x=Math.min(((map.getPosY()+map.getlength())/2),getDistance());
-			for (byte i=0; i<x;i++){
-				w.add(Richtung.SOUTH);
-			}
+				x=Math.min(((map.getPosY()+map.getlength())/2),getDistance());
+				for (byte i=0; i<x;i++){
+					w.add(Richtung.SOUTH);
+				}
 			goWay(w);
-			return;
+				return;
 			
 			case WEST:
-			w.add(Richtung.WEST);
-			goWay(w);
-			return;
+				if (getDistance()!=0){
+				w.add(Richtung.WEST);}
+				goWay(w);
+				return;
 			
 			case NORTH:
-			w.add(Richtung.NORTH);
-			goWay(w);
-			return;
+				if (getDistance()!=0){
+				w.add(Richtung.NORTH);}
+				goWay(w);
+				return;
 			
 		}
 	}
 	void goWay(ArrayList<Richtung> d){
 		if (d.size()==0){
-			//change dir
+			d.add(0, changeDir(map.getPosRi()));
 		}
 		else{
 			
 			Richtung ri[] = new Richtung[d.size()]; 
 			bewegungen.goWay(d.toArray(ri));
 		}
+	}
+	private Richtung changeDir(Richtung d){
+		switch (d){
+		case WEST:
+			return Richtung.NORTH;
+		case NORTH:
+			return Richtung.EAST;
+		case EAST:
+			return Richtung.SOUTH;
+		case SOUTH:
+			return Richtung.WEST;
+		}
+		return null;
 	}
 	/**
 	 * Only called if isFire==true. Moves the robot towards fire.
